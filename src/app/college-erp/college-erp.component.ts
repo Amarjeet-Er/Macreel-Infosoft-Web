5cd126d4c9c674178ed225fd502b03fc2369fb9f
Amarjeet Kumar <bidupur8544@gmail.com.com>
2025-06-13 18:59:59 +0530

    some change

--- src/app/college-erp/college-erp.component.ts
+++ src/app/college-erp/college-erp.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-college-erp',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./college-erp.component.scss']
 })
 export class CollegeErpComponent {
+  demoForm: FormGroup;
+  submitted = false;
 
+  constructor(private fb: FormBuilder) {
+    this.demoForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      contact: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      subject: [''],
+      message: ['']
+    });
+  }
+
+  onSubmit(): void {
+    if (this.demoForm.valid) {
+      console.log('Form Data:', this.demoForm.value);
+      this.submitted = true;
+      this.demoForm.reset();
+    }
+  }
 }
