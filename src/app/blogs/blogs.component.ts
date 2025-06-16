3671fb932d8d5e08b6227b551bceb0bc2b8789ae
Amarjeet Kumar <bidupur8544@gmail.com.com>
2025-06-16 14:17:25 +0530

    some change

--- src/app/blogs/blogs.component.ts
+++ src/app/blogs/blogs.component.ts
@@ -7,11 +7,6 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrls: ['./blogs.component.scss'],
 })
 export class BlogsComponent {
-  breaksPoint = {
-    320: { slidesPerView: 1, spaceBetween: 10 },
-    768: { slidesPerView: 2, spaceBetween: 10 },
-    1024: { slidesPerView: 3, spaceBetween: 10 },
-  };
 
   constructor(private _meta: Meta, private _title: Title) {
     this._title.setTitle('Blogs | Best IT Solution Company |Macreel Infosoft');
@@ -38,4 +33,29 @@ export class BlogsComponent {
       { name: 'coverage', content: 'Worldwide' },
     ]);
   }
+
+
+  blogs = [
+    {
+      image: '../../../assets/e-commerce.png',
+      alt: 'Top Ecommerce Website Development',
+      title: 'Top e-commerce Website Development Company in 2024.',
+      description:
+        'In the ever-evolving world of eCommerce, having a cutting-edge online store is essential for success. As we move through 2024, businesses need more than just a website—they need a platform that drives sales, engages customers, and adapts to emerging trends.'
+    },
+    {
+      image: '../../../assets/web-app.png',
+      alt: 'Dropshipping App Development',
+      title: 'How to develop a custom Dropshipping App',
+      description:
+        'In the rapidly growing world of e-commerce, dropshipping has emerged as a popular model for entrepreneurs. Here’s what you need to know about building your own custom app.'
+    },
+    {
+      image: '../../../assets/mobile-apps.png',
+      alt: 'Web Application Development',
+      title: 'A complete guide to web application development',
+      description:
+        'Web applications play a crucial role in transforming business operations. Here’s a guide to the various types and how to choose the right one for your business.'
+    }
+  ];
 }
